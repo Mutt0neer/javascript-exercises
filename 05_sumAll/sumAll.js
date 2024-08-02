417f8702857b09769e68718cd504407dc729cf78
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-02 19:35:45 -0400

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(val1, val2) {
+    let result=0
+    if(val1!==Math.floor(val1)||val2!==Math.floor(val2)||Number.isNaN(val1)||Number.isNaN(val2)||val1<0||val2<0){
+        return("ERROR");
+    }
+    if (val1<val2){
+        for(let i=val1; i<=val2;i++){
+            result+=i;
+        }
+    } else {
+        for(let i=val2; i<=val1;i++){
+            result+=i;
+        }
+    }
+    return(result);
 };
 
 // Do not edit below this line
