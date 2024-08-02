70548da1b52338e78640840186542c6ac78c2e3e
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-02 18:26:24 -0400

    02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, numtimes) {
+    let ret=""
+    if (numtimes<0){
+        return("ERROR")
+    }
+    for (i=0;i<numtimes;i++){
+        ret +=word;
+    }
 
+    return(ret);
 };
 
 // Do not edit below this line
