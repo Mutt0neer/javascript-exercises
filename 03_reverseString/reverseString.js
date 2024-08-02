d76b380203c7429768bfbeafd42370a33aae4f88
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-02 18:46:02 -0400

    03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("")
+    let reversed = []
+    for(i=arr.length-1;i>=0;i--){
+        reversed.push(arr[i])
+    } 
+    return(reversed.join(""))
 };
 
 // Do not edit below this line
