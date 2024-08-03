183a79d79cd3faaa82c4a0b09327715ecaebd782
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-02 23:02:17 -0400

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  console.log(Math.round(1.3))
+  return(Math.round(((F-32)*(5/9))*10)/10)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  console.log(Math.round(1.3))
+  return(Math.round((C*(9/5)+32)*10)/10)
 };
 
 // Do not edit below this line
