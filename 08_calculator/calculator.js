d8c937d9540b4d843a41a75d7586ad9befefba02
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-20 20:52:38 -0400

    08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(...nums) {
+  let add = 0;
+	nums.forEach(element => {
+    add+=element;
+  });
+  return(add);
 };
 
-const subtract = function() {
-	
+const subtract = function(init, sub) {
+	return(init-sub)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum =0;
+  arr.forEach(element => {
+    sum+=element;
+  });
+  return(sum);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(element => {
+    product*=element
+  });
+  return(product);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	let tot;
+  tot=Math.pow(base, power);
+  return(tot);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact=1;
+  while(num>1){
+    fact*=num;
+    num--;
+  }
+  return(fact)
 };
 
 // Do not edit below this line
