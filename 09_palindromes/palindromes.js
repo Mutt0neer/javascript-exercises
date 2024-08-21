1ea4ff3f0a5eddaa725d076d199f1bc680a6d22f
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-20 21:22:33 -0400

    09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    str = str.toLowerCase();
+    str = str.split("")
+    let norm = str.filter((char) => char!=="!"&&char!==","&&char!=="."&&char!==" ");
+    let normcopy=norm;
+    let reversed=[];
+    let x=0
+    let bool=true;
+    for(let i = norm.length-1; i>=0;i--){
+        reversed[x]=norm[i];
+        x++;
+    };
 
+    for(let o=0; o<norm.length;o++){
+        if(reversed[o]!==norm[o]){
+            bool=false;
+        }
+    }
+
+    console.log(norm);
+    console.log(reversed);
+    return(bool);
 };
 
 // Do not edit below this line
