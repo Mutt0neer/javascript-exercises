723055a67342d9b9b2289bc9101a4bd3304bc9cc
Mutt0neer <milluzzi26@students.hopkins.edu>
2024-08-20 21:41:11 -0400

    10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(leng) {
+let double=0;
+let single=1;
+let num=0;
+leng=Number(leng);
+if(leng<0){
+    return("OOPS")
+}
+if(leng==1){
+    num=1
+}
+for(i=1;i<=leng-1;i++){
+    num=single+double;
+    double=single;
+    single=num;
+    console.log(num);
+}
+return(num);
 };
 
 // Do not edit below this line
